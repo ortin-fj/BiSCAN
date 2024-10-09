1d0dcf9135ded199dedf9b66f7914238298bf67a
ortin-fj <ortin.fj@gmail.com>
2024-10-09 22:29:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="BiSCAN_module",                # Nombre de tu módulo/paquete
-    version="1.0",                   # Versión del paquete
-    packages=find_packages(),        # Encuentra automáticamente todos los subdirectorios con __init__.py
+    name="BiSCAN_module",            
+    version="1.0",                  
+    packages=find_packages(),      
     entry_points={
         'console_scripts': [
-            'biscan=BiSCAN_module.biscan_main:main',  # Aquí se asume que en jaja.py tienes una función llamada main()
+            'biscan=BiSCAN_module.biscan_main:main', 
         ],
     },
 )
